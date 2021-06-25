0e85a9de68169e2e026c507ce1c08ad1c68bd413
prajjwal poudel <iamprazol@gmail.com>
2021-06-25 17:21:52 +0545

    Day - 005 ( Part 2 : FooterTop section completed )

---
+++ src/components/common/footer/footerTop/FooterTop.js
@@ -0,0 +1,66 @@
+import React from "react";
+import IconContainer from "../../iconContainer/IconContainer";
+import "./FooterTop.css";
+import PaymentIcon from "@material-ui/icons/Payment";
+import AccountBalanceIcon from "@material-ui/icons/AccountBalance";
+import CardGiftcardIcon from "@material-ui/icons/CardGiftcard";
+import AssignmentTurnedInOutlinedIcon from "@material-ui/icons/AssignmentTurnedInOutlined";
+import AppsIcon from "@material-ui/icons/Apps";
+import ContactSupportTwoToneIcon from "@material-ui/icons/ContactSupportTwoTone";
+
+const FooterTop = () => {
+  return (
+    <section class="rfb-footer-top section-padding bg-black">
+      <div className="icon-box">
+        <IconContainer
+          icon={<PaymentIcon />}
+          colorClass="text-red"
+          fontSizeClass="icon-large"
+        />
+        <h4 className="text-white">{"100% Payment Secured"}</h4>
+      </div>
+      <div className="icon-box">
+        <IconContainer
+          icon={<AccountBalanceIcon />}
+          colorClass="text-red"
+          fontSizeClass="icon-large"
+        />
+        <h4 className="text-white">{"Support lots of Payments"}</h4>
+      </div>
+      <div className="icon-box">
+        <IconContainer
+          icon={<ContactSupportTwoToneIcon />}
+          colorClass="text-red"
+          fontSizeClass="icon-large"
+        />
+        <h4 className="text-white">{"24 hours / 7 days Support"}</h4>
+      </div>
+      <div className="icon-box">
+        <IconContainer
+          icon={<CardGiftcardIcon />}
+          colorClass="text-red"
+          fontSizeClass="icon-large"
+        />
+        <h4 className="text-white">{"Free Delivery with Rs.5000"}</h4>
+      </div>
+      <div className="icon-box">
+        <IconContainer
+          icon={<AssignmentTurnedInOutlinedIcon />}
+          colorClass="text-red"
+          fontSizeClass="icon-large"
+        />
+        <h4 className="text-white">{"Best Price Guaranteed"}</h4>
+      </div>
+      <div className="icon-box">
+        <IconContainer
+          icon={<AppsIcon />}
+          colorClass="text-red"
+          fontSizeClass="icon-large"
+        />
+        <h4 className="text-white">{"Mobile Apps Ready"}</h4>
+      </div>
+    </section>
+  );
+};
+
+export default FooterTop;
--- src/components/home/homePage/HomePage.js
+++ src/components/home/homePage/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import BrowseByCategory from "../browseByCategory/BrowseByCategory";
 import RestaurantList from "../restaurantList/RestaurantList";
+import FooterTop from "../../common/footer/footerTop/FooterTop";
 
 const HomePage = () => {
   return (
     <div className="container-fluid">
       <BrowseByCategory />
       <RestaurantList />
+      <FooterTop />
     </div>
   );
 };
